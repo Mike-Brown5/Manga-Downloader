361e3f4dd0c97117983746257ca3c0e47079a386
Mike-Brown5 <mikeaxe5@outlook.com>
2022-04-23 00:09:25 +0200

    adding an exception

--- App.py
+++ App.py
@@ -8,11 +8,14 @@ driver.get("https://w13.mangafreak.net/Manga/Solo_Leveling")
 
 i = 8
 
-while True:
+try:
+    while True:
 
-    downlaod = driver.find_element_by_xpath(f'//a[contains(@href,"https://images.mangafreak.net/downloads/Solo_Leveling_{i}")]')
-    downlaod.click()
-    i = i + 1
+        downlaod = driver.find_element_by_xpath(f'//a[contains(@href,"https://images.mangafreak.net/downloads/Solo_Leveling_{i}")]')
+        downlaod.click()
+        i = i + 1
 
-    time.sleep(5)
-
+        time.sleep(5)
+except:
+    downlaod.quit()
+    
