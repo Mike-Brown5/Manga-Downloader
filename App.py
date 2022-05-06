815e777477d7f80a364473307dab3e96148587f8
Mike-Brown5 <mikeaxe5@outlook.com>
2022-05-07 01:34:22 +0200

    debuging

--- App.py
+++ App.py
@@ -28,3 +28,4 @@ try:
 except:
     downlaod.quit()
 
+export
--- zip try.py
+++ zip try.py
@@ -1,9 +1,16 @@
 from zipfile import ZipFile
-from .App import link
+import os
+
+link = input("Enter ur link for the manga: ")
+
+link = link[33:]
 
 
+q = int(input("enter the last chapter you have: "))
 
-with ZipFile(f'/home/mike/Downloads/{link}_', 'r') as zipObj:
+i = q + 1
+
+with ZipFile(f'/home/mike/Downloads/{link}_{i}.zip', 'r') as zipObj:
    # Extract all the contents of zip file in different directory
    zipObj.extractall(f'/home/mike/Downloads/{link}/{link}_{i}')
    print('done')
