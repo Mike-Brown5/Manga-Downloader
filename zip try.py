e57cfaf7105a7406986bee001f961f50af1510a6
Mike-Brown5 <mikeaxe5@outlook.com>
2022-05-07 01:21:07 +0200

    adding the extartall method

--- zip try.py
+++ zip try.py
@@ -1,1 +1,9 @@
 from zipfile import ZipFile
+from .App import link
+
+
+
+with ZipFile(f'/home/mike/Downloads/{link}_', 'r') as zipObj:
+   # Extract all the contents of zip file in different directory
+   zipObj.extractall(f'/home/mike/Downloads/{link}/{link}_{i}')
+   print('done')
